a08e1dc105da2b54a9915b2dbbd446389b783b6d
Baris Senyerli <barissenyerli@gmail.com>
2022-08-06 15:02:07 +0300

    delete unnode img tag

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,11 +6,11 @@ const MovieList = (props) => {
 		<>
 			{props.movies.map((movie, index) => (
 				<div className='image-container d-flex justify-content-start m-3'>
-					<img src={movie.Poster} alt='movie'></img>
+					<img src={movie.Poster} alt='movie' />
 					<div 
-					onClick={()=> props.handleFavouritesClick(movie)}
-					className='overlay d-flex align-items-center justify-content-centerr'>
-						<FavoriteComponent/>	
+						onClick={()=> props.handleFavouritesClick(movie)}
+						className='overlay d-flex align-items-center justify-content-centerr'>
+							<FavoriteComponent/>	
 					</div>
 				</div>
 			))}
